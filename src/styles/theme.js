a6cdea6374de844a1fb3c0931dcc3febe7f50d49
oranc <krani1004@gmail.com>
2023-08-24 10:54:41 +0900

    예약페이지 초기 업로드

---
+++ src/styles/theme.js
@@ -0,0 +1,33 @@
+const theme = {
+  mainGrayColor: '#333333',
+  mainSubColor: '#1768FF',
+  mainBgcolor: '#ffffff',
+  mainSubBgcolor: '#ffffff',
+  darkGrayColor: '#333333',
+  darkAccentColor: '#242428',
+  grayColor: '#999999',
+  lineColor: '#ECEDFC',
+  accentColor: '#C82020',
+
+  button: {
+    accentBgColor: '#C82020',
+    accentTextColor: '#ffffff',
+    baseBgColor: '#ffffff',
+    baseTextColor: '#333333',
+    baseBorderColor: '#333333'
+  },
+
+  book: {
+    stageColor: '#7D7D7D',
+  },
+
+  hr: {
+    baseColor: '#333333',
+  },
+
+  test: {
+    testColor: 'red'
+  }
+}
+
+export default theme;

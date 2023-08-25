a3f086debb6244dc34c93d9d3ab8a91533814bdd
leeheetea <132877394+leeheetea@users.noreply.github.com>
2023-08-25 17:31:18 +0900

    마이페이지 중첩 라우터로 분리

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ import AccountForm from "./page/AccountForm";
 import NotFound from "./page/NotFound";
 import BookMainPage from "./pages/book/BookMainPage";
 import MyPage from "./pages/MyPage";
-import MyPointPage from "./pages/MyPointPage";
+import MyPagePoint from "./components/MyPagePoint";
 import SearchPage from "./pages/SearchPage";
 
 import ChangeDatePage from "./pages/book/ChangeDatePage";
@@ -27,6 +27,7 @@ import ChooseSeatsPage from "./pages/book/ChooseSeatsPage";
 import DiscountPricePage from "./pages/book/DiscountPricePage";
 import CheckBooksPage from "./pages/book/CheckBooksPage";
 import PaymentPage from "./pages/book/PaymentPage";
+import TicketingCancle from "./components/TicketingCancle";
 
 function App() {
   return (
@@ -54,8 +55,10 @@ function App() {
               <Route path="4" element={<CheckBooksPage />}></Route>
               <Route path="5" element={<PaymentPage />}></Route>
             </Route>
-            <Route path="/mypage" element={<MyPage />}></Route>
-            <Route path="/mypage/point" element={<MyPointPage />}></Route>
+            <Route path="/mypage/:id?" element={<MyPage />}>
+              <Route path="ticketingcancle" element={<TicketingCancle />} />
+              <Route path="point" element={<MyPagePoint />}></Route>
+            </Route>
             <Route path="/search" element={<SearchPage />}></Route>
             <Route path="*" element={<NotFound />} />
           </Routes>

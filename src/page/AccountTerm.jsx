d93068633be40acbd1e16686996200c3db232ad6
endrmsehfakdcl <toyandy0205@gmail.com>
2023-08-28 01:03:20 +0900

    css

--- src/page/AccountTerm.jsx
+++ src/page/AccountTerm.jsx
@@ -17,17 +17,25 @@ const AccountTerm = () => {
     term7: false,
     term8: false,
     term9: false,
-    term10: false,
+    term10: false
   });
 
   const handleAllAgreeChange = () => {
-    setAllAgree(!allAgree);
 
     const updatedTermsChecked = {};
     for (const key in termsChecked) {
       updatedTermsChecked[key] = !allAgree;
     }
     setTermsChecked(updatedTermsChecked);
+
+    const updatedSubTermsChecked = {};
+    for (const key in subTermsChecked) {
+      updatedSubTermsChecked[key] = !allAgree;
+    }
+    setSubTermsChecked(updatedSubTermsChecked);
+
+    setAllAgree(!allAgree);
+    setMarketingTerm(!allAgree);
   }
 
   const handleTermChange = (term) => {
@@ -35,13 +43,13 @@ const AccountTerm = () => {
       ...termsChecked,
       [term]: !termsChecked[term]
     };
+    setTermsChecked(updatedTermsChecked);
 
     const isAllTermsChecked = Object.values(updatedTermsChecked).every(
       (checked) => checked);
-    setTermsChecked(updatedTermsChecked);
     setAllAgree(isAllTermsChecked);
-
   }
+
   /////////////////////////////////////////////////////////////////////
   const [marketingTerm, setMarketingTerm] = useState(false);
   const [subTermsChecked, setSubTermsChecked] = useState({
@@ -54,9 +62,9 @@ const AccountTerm = () => {
 
   const handleMarketingTerm = () => {
     const newMarketingTerm = !marketingTerm;
+    const updatedSubTermsChekced = {};
     setMarketingTerm(newMarketingTerm);
 
-    const updatedSubTermsChekced = {};
     for (const key in subTermsChecked) {
       updatedSubTermsChekced[key] = !marketingTerm;
     }
@@ -73,15 +81,13 @@ const AccountTerm = () => {
       (checked) => checked);
     setSubTermsChecked(updatedSubTermsChekced);
     setMarketingTerm(isAllSubTermsChecked);
+    if (!isAllSubTermsChecked) {
+      setTermsChecked({ ...termsChecked, term10: false });
+    }
   }
   ////////////////////////////////////////////////////////////////////////
   // console.log('termsChecked.term10: ', termsChecked.term10);
-  // console.log('marketingTerm: ', marketingTerm);
-  // useEffect(() => {
-  //   if (termsChecked.term10 === true) {
-  //     setMarketingTerm(true);
-  //   }
-  // }, [termsChecked.term10]);
+  // console.log('marketingTerm: ', marketingTerm); 
 
   const areAllRequiredTermsChecked = ['term1', 'term2', 'term3', 'term4', 'term5', 'term6', 'term7'].every((term) => termsChecked[term]);
 
@@ -280,6 +286,7 @@ const AccountTerm = () => {
         </div>
       </div>
     </div>
+
   )
 }
 

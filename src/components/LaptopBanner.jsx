36485250cb437711875f43baf875c970993a637b
leeheetea <132877394+leeheetea@users.noreply.github.com>
2023-08-28 09:21:03 +0900

    메인css, 링크 수정

--- src/components/LaptopBanner.jsx
+++ src/components/LaptopBanner.jsx
@@ -1,26 +1,35 @@
 import React from "react";
-import banner1 from "../img/banner1.png";
-import banner2 from "../img/banner2.png";
-import banner3 from "../img/banner3.png";
-import banner4 from "../img/banner4.png";
-import banner5 from "../img/banner5.png";
-import banner6 from "../img/banner6.png";
+import banner1Url from "../img/banner1.png";
+import banner2Url from "../img/banner2.png";
+import banner3Url from "../img/banner3.png";
+import banner4Url from "../img/banner4.png";
+import banner5Url from "../img/banner5.png";
+import banner6Url from "../img/banner6.png";
 import "../css/LaptopBanner.css";
+import { Link } from "react-router-dom";
 
 const LaptopBanner = () => {
-  const banner = [banner1, banner2, banner3, banner4, banner5, banner6];
+  const banner1 = { imgUrl: banner1Url, url: "1001" };
+  const banner2 = { imgUrl: banner2Url, url: "1002" };
+  const banner3 = { imgUrl: banner3Url, url: "1009" };
+  const banner4 = { imgUrl: banner4Url, url: "1106" };
+  const banner5 = { imgUrl: banner5Url, url: "1107" };
+  const banner6 = { imgUrl: banner6Url, url: "1109" };
+  const banners = [banner1, banner2, banner3, banner4, banner5, banner6];
 
   return (
     <div className="banner-container">
-      {banner.map((imageSrc, index) => (
-        <div
-          key={index}
-          className="banner-img"
-          style={{
-            background: `url(${imageSrc}) no-repeat center center`,
-            backgroundSize: "cover",
-          }}
-        ></div>
+      {banners.map((banner, index) => (
+        <Link to={"detailpage/" + banner.url}>
+          <div
+            key={index}
+            className="banner-img"
+            style={{
+              background: `url(${banner.imgUrl}) no-repeat center center`,
+              backgroundSize: "cover",
+            }}
+          ></div>
+        </Link>
       ))}
     </div>
   );

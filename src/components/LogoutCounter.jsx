767396a20bbc4f5b78e0314f1d078fc766bb9f1c
endrmsehfakdcl <toyandy0205@gmail.com>
2023-11-02 02:41:20 +0900

    LogoutWorker done

--- src/components/LogoutCounter.jsx
+++ src/components/LogoutCounter.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { logout } from '../page/ApiService';
 
 const LogoutCounter = () => {
@@ -6,38 +6,48 @@ const LogoutCounter = () => {
     const savedTime = localStorage.getItem("REMAINING_TIME");
     const initialTime = savedTime ? parseInt(savedTime, 10) : INIT_TIME;
     const [remainingTime, setRemainingTime] = useState(initialTime);
+    let worker;
 
     useEffect(() => {
-        const countDown = setInterval(() => {
-            setRemainingTime((prev) => {
-                if (prev === 1) {
-                    logout()
-                        .then(() => {
-                            window.location.href = "/";
-                        });
-                    return 0;
-                }
-                return prev - 1;
-            });
-        }, 1000);
-        return () => {
-            clearTimeout(countDown);
+        worker = new Worker('/workers/LogoutWorker.js');
+
+        const savedTime = localStorage.getItem("REMAINING_TIME");
+        if (savedTime) {
+            worker.postMessage({ type: 'setTime', time: parseInt(savedTime, 10) });
+        } else {
+            worker.postMessage({ type: 'setTime', time: INIT_TIME});
         }
-    }, [remainingTime]);
 
-    useEffect(() => {
-        localStorage.setItem("REMAINING_TIME", remainingTime); // remainingTime이 변경될 때마다 로컬 저장소에 저장
-    }, [remainingTime]);
+        worker.onmessage = (event) => {
+            if (event.data === 'logout') {
+                logout()
+                    .then(() => {
+                        window.location.href = "/";
+                    });
+            } else {
+                setRemainingTime(event.data);
+                localStorage.setItem("REMAINING_TIME", event.data);
+            }
+        };
+
+        return () => {
+            worker.terminate();
+        };
+    }, []);
 
     const resetINITTIME = () => {
         setRemainingTime(INIT_TIME);
+        if (worker) {
+            worker.postMessage({ type: 'setTime', time: INIT_TIME });
+        }
     }
 
     return (
         <div>
             <span>{`${Math.floor(remainingTime / 60)}:${remainingTime % 60 < 10 ? '0' : ''}${remainingTime % 60}`}</span>
             <span onClick={resetINITTIME} className='search-container-menu-text'>로그인 연장</span>
-        </div>);
+        </div>
+    );
 }
 
-export default LogoutCounter
+export default LogoutCounter;
